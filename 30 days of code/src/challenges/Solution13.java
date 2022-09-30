e08a373c7da4f6e204a59d40ee0f9738ad806ca6
Rahul P B <rahulpb2112@gmail.com>
2022-09-30 21:37:51 +0530

    removed a few commented messages

--- 30 days of code/src/challenges/Solution13.java
+++ 30 days of code/src/challenges/Solution13.java
@@ -69,26 +69,6 @@ class MyBook extends Book{
         System.out.println("Title: "+ title + "\nAuthor: " + author + "\nPrice: " + price);
     }
 }
-// Declare your class here. Do not use the 'public' access modifier.
-    // Declare the price instance variable
-    
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
-// End class
 
 public class Solution13 {
    
